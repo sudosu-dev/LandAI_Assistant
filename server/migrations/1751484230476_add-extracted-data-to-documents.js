0257104633c3ebecb855cccbe212e8fff4dfdd91
Matthew Sharp <sharpmatthew505@gmail.com>
2025-07-02 14:40:49 -0500

    refactored document and document-processing.service files to use AI extraction

---
+++ server/migrations/1751484230476_add-extracted-data-to-documents.js
@@ -0,0 +1,22 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.addColumns("documents", {
+    extracted_data: {
+      type: "jsonb",
+      notNull: false,
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropColumns("documents", ["extracted_data"]);
+};
