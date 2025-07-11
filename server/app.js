b9cb171a5648295025670bc77165f5c2137f6dc5
Matthew Sharp <sharpmatthew505@gmail.com>
2025-07-10 21:41:35 -0500

    fixed import for cors edit

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-import apiRouterV1 from "#api/index.js";
+import apiRouterV1 from "#api/index";
 
 const app = express();
 
